fc9bcdd8e33e0f434bfd90560182e8a3bc1adf11
Pavan <pavan9322@gmail.com>
2020-06-01 14:31:12 -0700

    Updated code for paypal payment integration

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,37 +1,35 @@
 from django.shortcuts import render
-from .models import Product, OrderItem, Order
+from .models import Product, OrderItem, Order, ShippingAddress
 from django.http import JsonResponse
 import json
+import datetime
+from django.views.decorators.csrf import csrf_exempt
+from .utils import cookieCart, cartData, guestOrder
 
 # Create your views here.
 
 def store(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        items = []
-        order = {'get_cart_total':0 , 'get_cart_items':0}
-    context = {'items':items, 'order': order}
-
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+    context = {'items':items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
+@csrf_exempt
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        items = []
-        order = {'get_cart_total':0 , 'get_cart_items':0}
-    context = {'items':items, 'order': order}
-    # context = {}
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+    context = {'items':items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
 
 
@@ -55,3 +53,30 @@ def updateItem(request):
     if orderItem.quantity <= 0:
         orderItem.delete()
     return JsonResponse('Item was added', safe=False)
+
+
+@csrf_exempt
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    else:
+        customer , order = guestOrder(request, data)
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+    if total == float(order.get_cart_total):
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer =customer,
+            order = order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            zipcode = data['shipping']['zipcode'],
+        )
+    return JsonResponse('Payment Completed', safe=False)
